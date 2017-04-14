ed3b2993320465278a45ba8753f3948ea6b3c432
batbrain7 <kumar.mohit983@gmail.com>
2017-04-15 03:56:26 +0530

    Done Scraping

--- Scraping/server.js
+++ Scraping/server.js
@@ -8,7 +8,7 @@ var fs = require('fs');
 app.get('/scrape',function(req,res) {
   // res.send('This is the demo page to scrape the web');
 
-  url = '';
+  url = 'http://www.imdb.com/title/tt3417422/?pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=2681270342&pf_rd_r=150YB6DTA9MWACRX7A1Z&pf_rd_s=center-4&pf_rd_t=64301&pf_rd_i=top-rated-indian-movies&ref_=in_india_ss_toprated_tt_2';
   request(url,function(error,response,html) {
     if(!error) {
       var $ = cheerio.load(html);
@@ -16,14 +16,27 @@ app.get('/scrape',function(req,res) {
       var title,release,rating;
       var json = {title : "",release:"",rating:""};
 
-      $('.header').filter(function() {
+      $('.title_wrapper').filter(function() {
         var data = $(this);
         title = data.children().first().text();
+        release  = data.children().first().last().children().text();
+        json.title = title;
+        json.release = release;
+      })
 
+      $('.star-box-giga-star').filter(function() {
+        var data = $(this);
+        rating = data.text();
+        json.rating = rating;
       })
     }
-  })
-});
+
+    fs.writeFile('outputjson',JSON.stringify(json,null,4),function(err) {
+      console.log('File saved successfully!');
+    })
+    res.send('Check your console!');
+  });
+})
 
 app.listen('8081');
 
